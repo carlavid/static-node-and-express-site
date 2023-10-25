72416e016d3511ad59a87d020241ff33422efd2f
Carla Vidales <carlavidales.ca@gmail.com>
2023-10-24 17:33:02 -0700

    Update pug templates, images and routes

--- app.js
+++ app.js
@@ -1,28 +1,37 @@
 const express = require("express");
+const { projects } = require("./data.json");
 const app = express();
 
 app.set("view engine", "pug");
+app.use(express.static('public'));
 
 /**
  * Set up routes
  */
 
-// create root route 
+/* GET index page */
 app.get("/", (req, res) => {
-    res.render("index");
+    res.render("index", { projects });
 })
 
-// create route to "about" page
+/* GET about page */
 app.get("/about", (req, res) => {
     res.render("about");
 })
 
-// create dynamic "project" routes
-app.get("/project", (req, res) => {
-    res.render("project");
+/* GET project page */
+app.get("/projects/:id", (req, res) => {
+    const projectId = req.params.id;
+    const project = projects.find(({ id }) => id === +projectId);
+    if (project) {
+        res.render("project", { project });
+    } else {
+        res.sendStatus(404);
+    }
+    
 })
 
-// create server
+/* Create server */
 app.listen(3000);
 
 
